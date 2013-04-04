30aefffaf19de277d63106ea6b2d9d7a16c64ee6
Marco Ceppi <marco@ceppi.net>
2013-04-04 15:25:32 -0400

    Don't use os.path.join for lp urls

--- scripts/lib/mr.py
+++ scripts/lib/mr.py
@@ -57,7 +57,7 @@ class Mr:
             self.config.add_section(name)
 
         self.config.set(name, 'checkout', "bzr branch %s %s" %
-                            (os.path.join(repository, name), name))
+                            ('/'.join([repository, name]), name))
 
         if checkout:
             self.checkout(name)
