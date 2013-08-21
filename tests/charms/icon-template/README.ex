00bb881e965a4ca852808d90768f09df13428c22
Marco Ceppi <marco@ceppi.net>
2013-08-21 00:04:38 -0400

    Added icon.svg for create and proof that icon.svg is not template version

---
+++ tests/charms/icon-template/README.ex
@@ -0,0 +1,5 @@
+Describe the intended usage of this charm and anything unique about how
+this charm relates to others here.
+
+Be sure to remove this section before submitting this to
+https://launchpad.net/charms for inclusion in the charm store.
