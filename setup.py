d2b901af8f899bceed5fb863dfaebbafac30c2bf
Marco Ceppi <marco@ceppi.net>
2013-08-27 17:40:42 -0400

    First pass at setuptools for charm-tools

--- setup.py
+++ setup.py
@@ -10,18 +10,37 @@ ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-__version__ = '0.0.3'
+__version__ = {
+  'charmhelpers': '0.0.3',
+  'charm-tools': '0.1.0'
+}
 
 
 setup(
     name='charmhelpers',
-    version=__version__,
+    version=__version__['charmhelpers'],
     packages=find_packages('helpers/python'),
     package_dir={'': 'helpers/python'},
     include_package_data=True,
     zip_safe=False,
     maintainer='Launchpad Yellow',
     description=('Helper functions for writing Juju charms'),
+    license='GPL v3',
+    url='https://launchpad.net/charm-tools',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ],
+)
+
+setup(
+    name='charm-tools',
+    version=__version__['charm-tools'],
+    packages=['charm-tools']
+    install_requires="launchpadlib",
+    maintainer='Marco Ceppi',
+    description=('Tools for maintaining Juju charms'),
     license='GPL v3',
     url='https://launchpad.net/charm-tools',
     classifiers=[
