fbb7be01baccae688280a80e99cd947216078fc5
Marco Ceppi <marco@ceppi.net>
2013-08-28 07:37:18 -0400

    Removed charmhelpers for now

--- setup.py
+++ setup.py
@@ -11,43 +11,40 @@ ez_setup.use_setuptools()
 
 from setuptools import setup, find_packages
 
-__version__ = {
-  'charmhelpers': '0.0.3',
-  'charm-tools': '0.1.0-rc1'
-}
+__version__ = '0.1.0-rc2'
 
 
-if True:
-    setup(
-        name='charmtools',
-        version=__version__['charm-tools'],
-        packages=['charmtools'],
-        install_requires=['launchpadlib'],
-        package_data={'charmtools': ['templates/*/*.*', 'templates/*/hooks/*']},
-        maintainer='Marco Ceppi',
-        description=('Tools for maintaining Juju charms'),
-        license='GPL v3',
-        url='https://launchpad.net/charm-tools',
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Developers",
-            "Programming Language :: Python",
+setup(
+    name='charmtools',
+    version=__version__,
+    packages=['charmtools'],
+    install_requires=['launchpadlib'],
+    package_data={'charmtools': ['templates/*/*.*', 'templates/*/hooks/*']},
+    maintainer='Marco Ceppi',
+    description=('Tools for maintaining Juju charms'),
+    license='GPL v3',
+    url='https://launchpad.net/charm-tools',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+    ],
+    entry_points={
+        'console_scripts': [
+            'charm = charmtools:main',
+            'juju-charm = charmtools:main',
+            'juju-charm-get = charmtools.get:main',
+            'juju-charm-getall = charmtools.getall:main',
+            'juju-charm-proof = charmtools.proof:main',
+            'juju-charm-create = charmtools.create:main',
+            'juju-charm-list = charmtools.list:main',
+            'juju-charm-promulgate = charmtools.promulgate:main',
+            'juju-charm-review = charmtools.review:main',
+            'juju-charm-review-queue = charmtools.review_queue:main',
+            'juju-charm-search = charmtools.search:main',
+            'juju-charm-subscribers = charmtools.subscribers:main',
+            'juju-charm-unpromulgate = charmtools.unpromulgate:main',
+            'juju-charm-update = charmtools.update:main',
         ],
-        entry_points={
-            'console_scripts': [
-                'juju-charm = charmtools:main',
-                'juju-charm-get = charmtools.get:main',
-                'juju-charm-getall = charmtools.getall:main',
-                'juju-charm-proof = charmtools.proof:main',
-                'juju-charm-create = charmtools.create:main',
-                'juju-charm-list = charmtools.list:main',
-                'juju-charm-promulgate = charmtools.promulgate:main',
-                'juju-charm-review = charmtools.review:main',
-                'juju-charm-review-queue = charmtools.review_queue:main',
-                'juju-charm-search = charmtools.search:main',
-                'juju-charm-subscribers = charmtools.subscribers:main',
-                'juju-charm-unpromulgate = charmtools.unpromulgate:main',
-                'juju-charm-update = charmtools.update:main',
-            ],
-        },
-    )
+    },
+)
