eea1ea20d847fb47289ffd0e6a842ea322d359e0
Marco Ceppi <marco@ceppi.net>
2013-10-23 19:09:55 -0400

    Added bundle proof support

---
+++ charmtools/linter.py
@@ -0,0 +1,25 @@
+
+class Linter(object):
+    def __init__(self):
+        self.lint = []
+        self.exit_code = 0
+
+    def crit(self, msg):
+        """Called when checking cannot continue."""
+        self.err("FATAL: " + msg)
+
+    def err(self, msg):
+        global EXIT_CODE
+        self.lint.append("E: " + msg)
+        if self.exit_code < 200:
+            self.exit_code = 200
+
+    def info(self, msg):
+        """Ignorable but sometimes useful."""
+        self.lint.append("I: " + msg)
+
+    def warn(self, msg):
+        global EXIT_CODE
+        self.lint.append("W: " + msg)
+        if self.exit_code < 100:
+            self.exit_code = 100
