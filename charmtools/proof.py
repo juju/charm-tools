c1387890551bb0d5ebe0903bb075f4b434f19929
Marco Ceppi <marco@ceppi.net>
2013-11-04 15:56:41 -0500

    Move argument parsing to main, update proof signature so it looks more like Python and not something I threw together last minute

--- charmtools/proof.py
+++ charmtools/proof.py
@@ -24,7 +24,7 @@ from charms import Charm
 from cli import parser_defaults
 
 
-def get_args(args):
+def get_args(args=None):
     parser = argparse.ArgumentParser(
         description='Performs static analysis on charms and bundles')
     parser.add_argument('-n', '--offline', action='store_false',
@@ -37,29 +37,30 @@ def get_args(args):
     return args
 
 
-def proof(args=None):
-    args = get_args(args)
-    name = args.charm_name
-    if not args.bundle:
+def proof(path, is_bundle=False, with_remote=True, debug=False):
+    path = os.path.abspath(path)
+    if not is_bundle:
         try:
-            c = Charm(os.path.abspath(name))
+            c = Charm(path)
         except:
             try:
-                c = Bundle(os.path.abspath(name), args.debug)
+                c = Bundle(path, debug)
             except Exception as e:
-                return "FATAL: Not a Bundle or a Charm, can not proof", 1
+                return ["FATAL: Not a Bundle or a Charm, can not proof"], 1
     else:
         try:
-            c = Bundle(os.path.abspath(name), args.debug)
+            c = Bundle(path, debug)
         except Exception as e:
-            return "FATAL: %s" % e.strerror, 1
+            return ["FATAL: %s" % e.strerror], 1
 
-    lint, err_code = c.proof(args.offline)
+    lint, err_code = c.proof(with_remote)
     return lint, err_code
 
 
 def main():
-    lint, exit_code = proof()
+    args = get_args()
+    lint, exit_code = proof(args.charm_name, args.bundle, args.offline,
+                            args.debug)
     if lint:
         print "\n".join(lint)
     sys.exit(exit_code)
