c0a195a58c539461b9d9d7f75dcb79422f4ab95b
Marco Ceppi <marco@ceppi.net>
2013-12-12 12:19:58 -0500

    Make README first class citizen

--- charmtools/templates/charm/README.ex
+++ charmtools/templates/charm/README.ex
@@ -1,4 +1,4 @@
-## Overview
+# Overview
 
 Describe the intended usage of this charm and anything unique about how this charm relates to others here. 
 
@@ -8,7 +8,7 @@ Use this as a Markdown reference if you need help with the formatting of this RE
 
 This charm provides [service](http://example.com). Add a description here of what the service itself actually does.
 
-## Usage
+# Usage
 
 Step by step instructions on using the charm:
 
@@ -18,32 +18,31 @@ and so on. If you're providing a web service or something that the end user need
 
 You can then browse to http://ip-address to configure the service. 
 
-### Scale out Usage
+## Scale out Usage
 
 If the charm has any recommendations for running at scale, outline them in examples here. For example if you have a memcached relation that improves performance, mention it here. 
 
-### Known Limitations and Issues
+## Known Limitations and Issues
 
 This not only helps users but gives people a place to start if they want to help you add features to your charm. 
 
-## Configuration
+# Configuration
 
 The configuration options will be listed on the charm store, however If you're making assumptions or opinionated decisions in the charm (like setting a default administrator password), you should detail that here so the user knows how to change it immediately, etc.
 
-## Contact Information
+# Contact Information
 
 Though this will be listed in the charm store itself don't assume a user will know that, so include that information here:
 
-### Upstream Project Name
+## Upstream Project Name
 
 - Upstream website
 - Upstream bug tracker
 - Upstream mailing list or contact information
 - Feel free to add things if it's useful for users
 
-### Charm Contact
+## Charm Contact
 
 Though the charm store lists a maintainer, a URL, and where to report bugs you might want to use this area to expand on different contact information if you want. 
 
 * Be sure to remove the templated parts before submitting to https://launchpad.net/charms for inclusion in the charm store.
-
