dbb978f29f75ee8ad44cac056fe6f05cdc6b2142
Marco Ceppi <marco@ceppi.net>
2014-12-05 16:23:19 -0500

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='charm-tools',
-    version="1.4.1",
+    version="1.5.0",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=['launchpadlib', 'argparse', 'cheetah', 'pyyaml',
