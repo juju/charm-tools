6f90fe9c21e94f2bcd5a090624a2248e3b76af69
Jorge Castro <jorge@ubuntu.com>
2015-09-14 11:53:04 -0400

    Add dependencies and interfaces

--- charmtools/templates/charm/README.ex
+++ charmtools/templates/charm/README.ex
@@ -30,6 +30,14 @@ This not only helps users but gives people a place to start if they want to help
 
 The configuration options will be listed on the charm store, however If you're making assumptions or opinionated decisions in the charm (like setting a default administrator password), you should detail that here so the user knows how to change it immediately, etc.
 
+# Interfaces
+
+This section should document the charm's interfaces. Describe what the charm provides and requires.
+
+# External Dependencies
+
+Describe if the charm needs something from an external source, for example if it needs a URL config flag set to fetch something. Things like needing PyPy, github, an http resource or any other remote service that the charm needs in order to work.
+
 # Contact Information
 
 Though this will be listed in the charm store itself don't assume a user will know that, so include that information here:
