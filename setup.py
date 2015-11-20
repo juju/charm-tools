3ae864a2b38ec9867a036144c1ed6cb12380e449
Marco Ceppi <marco@ceppi.net>
2015-11-20 10:29:59 -0500

    virtualenv is a dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
                       'pycrypto', 'paramiko', 'bzr', 'requests',
                       'charmworldlib', 'blessings', 'ruamel.yaml',
                       'pathspec', 'otherstuf', "path.py",
-                      "jujubundlelib", "pip"],
+                      "jujubundlelib", "pip", 'virtualenv'],
     include_package_data=True,
     maintainer='Marco Ceppi',
     maintainer_email='marco@ceppi.net',
