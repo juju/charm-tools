2aaf55ec041eb44b062dd5a497e75ae5b2d5d3b7
Charles Butler <chuck@dasroot.net>
2016-02-05 14:50:40 +0100

    Adjusted the deep merge operation to support merging of lists

--- charmtools/utils.py
+++ charmtools/utils.py
@@ -52,6 +52,8 @@ def deepmerge(dest, src):
     for k, v in src.iteritems():
         if dest.get(k) and isinstance(v, dict):
             deepmerge(dest[k], v)
+        elif dest.get(k) and isinstance(v, list):
+            dest[k].extend(v)
         else:
             dest[k] = copy.deepcopy(v)
     return dest
@@ -59,6 +61,7 @@ def deepmerge(dest, src):
 
 def delete_path(path, obj):
     """Delete a dotted path from object, assuming each level is a dict"""
+    # TODO: Support lists
     parts = path.split('.')
     for p in parts[:-1]:
         obj = obj[p]
