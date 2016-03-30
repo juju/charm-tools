2ca09b5ab3ca3e000067b2a8dc17bffd7be0e985
Marco Ceppi <marco@ceppi.net>
2016-03-29 23:32:30 -0400

    display charm version if possible fixes #123

--- charmtools/version.py
+++ charmtools/version.py
@@ -1,10 +1,27 @@
 
 import pkg_resources
 
+def charm_version():
+    try:
+        from apt.cache import Cache
+        charm_vers = Cache()['charm'].versions
+        for v in charm_vers:
+            if v.is_installed:
+                charm_ver = v.version
+                break
+    except ImportError:
+        charm_ver = 'unavailable'
+    except:
+        charm_ver = 'error'
+
+    return charm_ver
+
 
 def main():
     version = pkg_resources.get_distribution("charm-tools").version
-    print "%s %s" % ('charm-tools', version)
+
+    print "charm %s" % charm_version()
+    print "charm-tools %s" % version
 
 
 if __name__ == '__main__':
