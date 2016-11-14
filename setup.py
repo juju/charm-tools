01191ce2cc21ed742639a1aaddd95bc34d2e73ab
Marco Ceppi <marco@ceppi.net>
2016-11-14 13:08:24 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='charm-tools',
-    version="2.1.6",
+    version="2.1.7",
     packages=find_packages(
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     install_requires=[
