00d72a965838ef66e0d62de22d23110f6fd6042c
Cory Johns <johnsca@gmail.com>
2019-01-24 12:01:33 -0500

    Improve get_revision logic

--- charmtools/fetchers.py
+++ charmtools/fetchers.py
@@ -107,19 +107,21 @@ class Fetcher(object):
         return match.groupdict() if match else {}
 
     def get_revision(self, dir_):
-        dirlist = os.listdir(dir_)
-        if '.bzr' in dirlist:
-            rev_info = check_output('bzr revision-info', cwd=dir_)
-            return rev_info.decode('utf8').strip().split()[1]
-        elif '.git' in dirlist:
-            rev_info = check_output('git rev-parse HEAD', cwd=dir_)
-            return rev_info.decode('utf8').strip()
-        elif '.hg' in dirlist:
-            rev_info = check_output(
-                "hg log -l 1 --template '{node}\n' -r .", cwd=dir_)
-            return rev_info.decode('utf8').strip()
-        elif hasattr(self, 'revision'):
+        if self.revision:
             return self.revision
+        for cmd in ("git rev-parse HEAD",
+                    "bzr revision-info",
+                    "hg log -l 1 --template '{node}\n' -r ."):
+            try:
+                rev_info = check_output(cmd, cwd=dir_)
+                if cmd.startswith('git'):
+                    return rev_info.decode('utf8').strip()
+                if cmd.startswith('bzr'):
+                    return rev_info.decode('utf8').strip().split()[1]
+                if cmd.startswith('hg'):
+                    return rev_info.decode('utf8').strip()
+            except FetchError:
+                continue
         else:
             return None
 
